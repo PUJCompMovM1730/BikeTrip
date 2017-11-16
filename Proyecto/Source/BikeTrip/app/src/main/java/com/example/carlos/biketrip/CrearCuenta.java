680498d04b0fc9a6cfc0e8019e57e9a1afce6e9d
Carlos Rodríguez <andresrod1225@gmail.com>
2017-11-16 01:07:33 -0500

    mejora en verificacion de campos de fecha en creacion de cuenta y actualizacion de perfil

--- Proyecto/Source/BikeTrip/app/src/main/java/com/example/carlos/biketrip/CrearCuenta.java
+++ Proyecto/Source/BikeTrip/app/src/main/java/com/example/carlos/biketrip/CrearCuenta.java
@@ -2,12 +2,13 @@ package com.example.carlos.biketrip;
 
 import android.content.Intent;
 import android.icu.text.SimpleDateFormat;
-import android.icu.util.Calendar;
 import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
 import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.*;
 import android.view.inputmethod.EditorInfo;
@@ -39,6 +40,7 @@ import com.google.firebase.storage.UploadTask;
 
 import java.text.ParseException;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
@@ -76,6 +78,7 @@ public class CrearCuenta extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crear_cuenta);
 
+
         posSpinner = -1;
         spinner = (Spinner)findViewById(R.id.spinnerTipoUsuario);
         bcrear = (Button)findViewById(R.id.btnConfirmCrear);
@@ -85,6 +88,8 @@ public class CrearCuenta extends AppCompatActivity {
         mUserPassword = (EditText)findViewById(R.id.CCPassword);
         mUserName= (EditText)findViewById(R.id.CCNombre);
         mUserLastName = (EditText)findViewById(R.id.CCApellido);
+
+
         mFechaNac = (EditText) findViewById(R.id.CCFDN);
 
 
@@ -135,6 +140,8 @@ public class CrearCuenta extends AppCompatActivity {
             }
         });
 
+
+        validateDate();
 
         bcrear.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -191,6 +198,7 @@ public class CrearCuenta extends AppCompatActivity {
         }
 
         String fechaNac = mFechaNac.getText().toString();
+
         if	(TextUtils.isEmpty(fechaNac))	 {
             mFechaNac.setError("Required.");
             valid	=	false;
@@ -198,18 +206,87 @@ public class CrearCuenta extends AppCompatActivity {
         else	{
 
 
-            int edad = Integer.valueOf(mFechaNac.getText().toString());
-
-            if(edad<12 || edad>90){
-                mFechaNac.setError("Invalid");
+            String s = fechaNac.replace("/","");
+            if (!s.matches("[0-9]+")) {
                 valid = false;
+                mFechaNac.setError("Incorrect.");
             }
-
-            else mFechaNac.setError(null);
         }
 
 
         return	valid;
+    }
+
+    public void validateDate(){
+
+        TextWatcher tw = new TextWatcher() {
+
+            private String current = "";
+            private String ddmmyyyy = "DDMMYYYY";
+            private Calendar cal = Calendar.getInstance();
+
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (!s.toString().equals(current)) {
+                    String clean = s.toString().replaceAll("[^\\d.]|\\.", "");
+                    String cleanC = current.replaceAll("[^\\d.]|\\.", "");
+
+                    int cl = clean.length();
+                    int sel = cl;
+                    for (int i = 2; i <= cl && i < 6; i += 2) {
+                        sel++;
+                    }
+                    //Fix for pressing delete next to a forward slash
+                    if (clean.equals(cleanC)) sel--;
+
+                    if (clean.length() < 8){
+                        clean = clean + ddmmyyyy.substring(clean.length());
+                    }else{
+                        //This part makes sure that when we finish entering numbers
+                        //the date is correct, fixing it otherwise
+                        int day  = Integer.parseInt(clean.substring(0,2));
+                        int mon  = Integer.parseInt(clean.substring(2,4));
+                        int year = Integer.parseInt(clean.substring(4,8));
+
+                        mon = mon < 1 ? 1 : mon > 12 ? 12 : mon;
+                        cal.set(Calendar.MONTH, mon-1);
+                        year = (year<1900)?1900:(year>2100)?2100:year;
+                        cal.set(Calendar.YEAR, year);
+                        // ^ first set year for the line below to work correctly
+                        //with leap years - otherwise, date e.g. 29/02/2012
+                        //would be automatically corrected to 28/02/2012
+
+                        day = (day > cal.getActualMaximum(Calendar.DATE))? cal.getActualMaximum(Calendar.DATE):day;
+                        clean = String.format("%02d%02d%02d",day, mon, year);
+                    }
+
+                    clean = String.format("%s/%s/%s", clean.substring(0, 2),
+                            clean.substring(2, 4),
+                            clean.substring(4, 8));
+
+                    sel = sel < 0 ? 0 : sel;
+                    current = clean;
+                    mFechaNac.setText(current);
+                    mFechaNac.setSelection(sel < current.length() ? sel : current.length());
+                }
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+
+
+
+        };
+        mFechaNac.addTextChangedListener(tw);
+
     }
 
 
--- Proyecto/Source/BikeTrip/app/src/main/java/com/example/carlos/biketrip/PerfilPersonal.java
+++ Proyecto/Source/BikeTrip/app/src/main/java/com/example/carlos/biketrip/PerfilPersonal.java
@@ -14,6 +14,9 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
@@ -42,6 +45,7 @@ import com.google.firebase.storage.UploadTask;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Comparator;
 import java.util.List;
 
@@ -121,6 +125,7 @@ public class PerfilPersonal extends AppCompatActivity {
 
         loadDatosActual();
         loadMensajesActual();
+        validateDate();
 
         arrayAdapter = new ArrayAdapter<Mensaje>(this, android.R.layout.simple_list_item_1, listDatosAMostrar);
         listaMensajes.setAdapter(arrayAdapter);
@@ -150,29 +155,36 @@ public class PerfilPersonal extends AppCompatActivity {
             public void onClick(View view) {
 
                 try {
-                    actual.setNombre(PerfilPersonalNombre.getText().toString().trim());
-                    actual.setDescripcion(PerfilPersonalBio.getText().toString().trim());
-                    actual.setCorreo(PerfilPersonalCorreo.getText().toString().trim());
-                    actual.setEdad(PerfilPersonalEdad.getText().toString().trim());
 
-                    myRef=database.getReference(PATH_USERS+actual.getID()+"/nombre");
-                    myRef.setValue(PerfilPersonalNombre.getText().toString());
+                    if (validateForm()) {
+                        actual.setNombre(PerfilPersonalNombre.getText().toString().trim());
+                        actual.setDescripcion(PerfilPersonalBio.getText().toString().trim());
+                        actual.setCorreo(PerfilPersonalCorreo.getText().toString().trim());
+                        actual.setEdad(PerfilPersonalEdad.getText().toString().trim());
 
-                    myRef=database.getReference(PATH_USERS+actual.getID()+"/descripcion");
-                    myRef.setValue(PerfilPersonalBio.getText().toString());
+                        myRef=database.getReference(PATH_USERS+actual.getID()+"/nombre");
+                        myRef.setValue(PerfilPersonalNombre.getText().toString());
 
-                    myRef=database.getReference(PATH_USERS+actual.getID()+"/correo");
-                    myRef.setValue(PerfilPersonalCorreo.getText().toString());
+                        myRef=database.getReference(PATH_USERS+actual.getID()+"/descripcion");
+                        myRef.setValue(PerfilPersonalBio.getText().toString());
 
-                    myRef=database.getReference(PATH_USERS+actual.getID()+"/edad");
-                    myRef.setValue(PerfilPersonalEdad.getText().toString());
+                        myRef=database.getReference(PATH_USERS+actual.getID()+"/correo");
+                        myRef.setValue(PerfilPersonalCorreo.getText().toString());
+
+                        myRef=database.getReference(PATH_USERS+actual.getID()+"/edad");
+                        myRef.setValue(PerfilPersonalEdad.getText().toString());
 
 
-                    insertarPortadaEnStorageBD();
-                    insertarPefilEnStorageBD();
-                    opcionesCampos(false);
-                    Toast.makeText(getBaseContext(),"Cambios realizados correctamente",
-                            Toast.LENGTH_SHORT).show();
+                        insertarPortadaEnStorageBD();
+                        insertarPefilEnStorageBD();
+                        opcionesCampos(false);
+                        Toast.makeText(getBaseContext(),"Cambios realizados correctamente",
+                                Toast.LENGTH_SHORT).show();
+
+
+                    }
+
+
 
                 }catch (Exception e){Log.i("EXP ACEPTARCAMBIOS",e.toString());}
 
@@ -353,6 +365,126 @@ public class PerfilPersonal extends AppCompatActivity {
 
         AlertDialog dialog = myBuild.create();
         dialog.show();
+
+    }
+
+    public boolean validateForm(){
+
+        boolean valid	=	true;
+
+        String	email	=	PerfilPersonalCorreo.getText().toString();
+        if	(TextUtils.isEmpty(email))	{
+            PerfilPersonalCorreo.setError("Required.");
+            valid	=	false;
+        }	else	{
+            PerfilPersonalCorreo.setError(null);
+        }
+
+        String	name	=	PerfilPersonalNombre.getText().toString();
+        if	(TextUtils.isEmpty(name))	 {
+            PerfilPersonalNombre.setError("Required.");
+            valid	=	false;
+        }	else	{
+            PerfilPersonalNombre.setError(null);
+        }
+
+        String	bio	=	PerfilPersonalBio.getText().toString();
+        if	(TextUtils.isEmpty(bio))	 {
+            PerfilPersonalBio.setError("Required.");
+            valid	=	false;
+        }	else	{
+            PerfilPersonalBio.setError(null);
+        }
+
+        String fechaNac = PerfilPersonalEdad.getText().toString();
+
+        if	(TextUtils.isEmpty(fechaNac))	 {
+            PerfilPersonalEdad.setError("Required.");
+            valid	=	false;
+        }
+        else	{
+
+
+            String s = fechaNac.replace("/","");
+            if (!s.matches("[0-9]+")) {
+                valid = false;
+                PerfilPersonalEdad.setError("Incorrect.");
+            }
+        }
+
+
+        return	valid;
+    }
+
+    public void validateDate(){
+
+        TextWatcher tw = new TextWatcher() {
+
+            private String current = "";
+            private String ddmmyyyy = "DDMMYYYY";
+            private Calendar cal = Calendar.getInstance();
+
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (!s.toString().equals(current)) {
+                    String clean = s.toString().replaceAll("[^\\d.]|\\.", "");
+                    String cleanC = current.replaceAll("[^\\d.]|\\.", "");
+
+                    int cl = clean.length();
+                    int sel = cl;
+                    for (int i = 2; i <= cl && i < 6; i += 2) {
+                        sel++;
+                    }
+                    //Fix for pressing delete next to a forward slash
+                    if (clean.equals(cleanC)) sel--;
+
+                    if (clean.length() < 8){
+                        clean = clean + ddmmyyyy.substring(clean.length());
+                    }else{
+                        //This part makes sure that when we finish entering numbers
+                        //the date is correct, fixing it otherwise
+                        int day  = Integer.parseInt(clean.substring(0,2));
+                        int mon  = Integer.parseInt(clean.substring(2,4));
+                        int year = Integer.parseInt(clean.substring(4,8));
+
+                        mon = mon < 1 ? 1 : mon > 12 ? 12 : mon;
+                        cal.set(Calendar.MONTH, mon-1);
+                        year = (year<1900)?1900:(year>2100)?2100:year;
+                        cal.set(Calendar.YEAR, year);
+                        // ^ first set year for the line below to work correctly
+                        //with leap years - otherwise, date e.g. 29/02/2012
+                        //would be automatically corrected to 28/02/2012
+
+                        day = (day > cal.getActualMaximum(Calendar.DATE))? cal.getActualMaximum(Calendar.DATE):day;
+                        clean = String.format("%02d%02d%02d",day, mon, year);
+                    }
+
+                    clean = String.format("%s/%s/%s", clean.substring(0, 2),
+                            clean.substring(2, 4),
+                            clean.substring(4, 8));
+
+                    sel = sel < 0 ? 0 : sel;
+                    current = clean;
+                    PerfilPersonalEdad.setText(current);
+                    PerfilPersonalEdad.setSelection(sel < current.length() ? sel : current.length());
+                }
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+
+
+
+        };
+        PerfilPersonalEdad.addTextChangedListener(tw);
 
     }
 
